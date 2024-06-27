6a5cee5e7538b4e68406c3204831fbcd0c43fa60
Chuma Okoro <chuma.okoro76@gmail.com>
2024-06-26 23:20:44 -0400

    init python modules and add some barebones unit tests

---
+++ server/tests/test_getEchoChamberStatus.py
@@ -0,0 +1,13 @@
+import pytest
+from server.controllers.getEchoChamberStatus import validateInput
+
+
+def test_validateInput():
+    validateInput("abc", "youtube")
+    assert True
+
+    with pytest.raises(Exception):
+        validateInput("abc", None)
+    
+    with pytest.raises(Exception):
+        validateInput(None, "youtube")
