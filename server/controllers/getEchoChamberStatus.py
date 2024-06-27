115f4bf2c83472db43988795528c57ea45c8be3c
Chuma Okoro <chuma.okoro76@gmail.com>
2024-06-26 20:20:40 -0400

    add boilerplate for method that takes/validates input and responds with json

--- server/controllers/getEchoChamberStatus.py
+++ server/controllers/getEchoChamberStatus.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import google.generativeai as genai
 
 
@@ -7,3 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/getEchoChamberStatus")
+def get_echo_chamber_status():
+    identifier = request.args.get('identifier')
+    chamber_type = request.args.get('chamber_type')
+    validateInput(identifier, chamber_type)
+
+    return {
+        "isChamber": True,
+        "chamberLabel": "right-wing",
+        "chamberMagnitude": 9,
+        "chamberReasoning": "Because I said so"
+    }
+
+def validateInput(identifier, chamber_type) -> None:
+    if identifier is None or chamber_type is None:
+        raise Exception(f"Empty input! identifier: {identifier} || chamber_type: {chamber_type}")
+    else:
+        return
