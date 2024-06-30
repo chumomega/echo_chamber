e2ad999fcd02b9072bebf1273ac7a45b7259cde2
Chuma Okoro <chuma.okoro76@gmail.com>
2024-06-30 14:48:00 -0400

    remove basic functionsg

--- server/controllers/basic_functions.py
+++
@@ -1,6 +0,0 @@
-from IPython.display import Markdown
-import textwrap
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
