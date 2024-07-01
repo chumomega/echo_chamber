4d7de07c3c55201655b43119c047b47b35efe44f
Chuma Okoro <chuma.okoro76@gmail.com>
2024-07-01 19:20:25 -0400

    add dockerfile and configure for runs via gunicorn

--- server/main.py
+++ server/main.py
@@ -5,5 +5,4 @@ from controllers.getEchoChamberStatus import echo_chamber_info_routes
 app = Flask(__name__)
 app.register_blueprint(echo_chamber_info_routes)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(environ.get("PORT", 8080)))
+# Gunicorn is used to run server
