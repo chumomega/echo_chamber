86be664be0c0f8b91d7c0098afa098094ccba630
Chuma Okoro <chuma.okoro76@gmail.com>
2024-07-03 20:17:36 -0400

    add todo

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ from controllers.getEchoChamberStatus import echo_chamber_info_routes
 import firebase_admin
 
 app = Flask(__name__)
+# TODO - add credentials. by default it finds credentials if you use cloud run to host
 firebase_admin.initialize_app(options = {"databaseURL": "https://echo-chamber-427700-default-rtdb.firebaseio.com/"})
 app.register_blueprint(echo_chamber_info_routes)
 
