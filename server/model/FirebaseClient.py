19c85e138cb8a53e044f88af02172258cd359955
Chuma Okoro <chuma.okoro76@gmail.com>
2024-07-09 07:07:01 -0400

    store comments in db

--- server/model/FirebaseClient.py
+++ server/model/FirebaseClient.py
@@ -1,5 +1,6 @@
 from firebase_admin import db
 from model.Chamber import Chamber
+from model.Comment import Comment
 from model.ChamberType import ChamberType
 
 CHAMBERS_TABLE = "tables/chambers"
@@ -16,6 +17,15 @@ class FirebaseClient:
     def add_chamber(self, chamber: Chamber, chamber_type: ChamberType) -> None:
         chamber_x_ref = db.reference(CHAMBERS_TABLE).child(chamber_type.value).child(chamber.get_id())
         chamber_x_ref.set(chamber.get_json_body())
+    
+    def add_chamber_member(self, chamber_identifier: str, chamber_type: ChamberType, comment: Comment) -> None:
+        # Store Comment data
+        comments_ref = db.reference(COMMENTS_TABLE).child(chamber_type.value).child(comment.get_id())
+        comments_ref.set(comment.get_json_body())
+
+        # Associate comment data with chamber table
+        chamber_members_ref = db.reference(CHAMBER_MEMBERS_TABLE).child(chamber_type.value).child(chamber_identifier)
+        chamber_members_ref.child(comment.get_id()).set(True)
 
     def get_chamber(self, identifier: str, chamber_type: ChamberType) -> Chamber:
         ref = db.reference(CHAMBERS_TABLE + "/{}/{}".format(chamber_type.value, identifier))
