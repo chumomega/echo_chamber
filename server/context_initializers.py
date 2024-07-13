c5e817310ce67f87bab5ca7646ab27c174ff4585
Chuma Okoro <chuma.okoro76@gmail.com>
2024-07-13 14:05:02 -0400

    black linting

--- server/context_initializers.py
+++ server/context_initializers.py
@@ -6,7 +6,7 @@ from model.GeminiClient import GeminiClient
 
 # add firebase client to Flask application context so only 1 instance of firebase running per Flask request
 def get_firebase() -> FirebaseClient:
-    if 'firebase' not in g:
+    if "firebase" not in g:
         g.firebase = FirebaseClient()
 
     return g.firebase
@@ -14,15 +14,15 @@ def get_firebase() -> FirebaseClient:
 
 # add youtube client to Flask application context so only 1 instance of youtube running per Flask request
 def get_youtube() -> YoutubeClient:
-    if 'youtube' not in g:
+    if "youtube" not in g:
         g.youtube = YoutubeClient()
 
     return g.youtube
 
+
 # add gemini client to Flask application context so only 1 instance of gemini running per Flask request
 def get_gemini() -> GeminiClient:
-    if 'gemini' not in g:
+    if "gemini" not in g:
         g.gemini = GeminiClient()
 
     return g.gemini
-
--- server/model/Chamber.py
+++ server/model/Chamber.py
@@ -1,5 +1,6 @@
 import time
 
+
 class Chamber:
     created_timestamp = None
     description = None
@@ -9,14 +10,14 @@ class Chamber:
     title = None
 
     def __init__(
-        self, 
-        id: str, 
+        self,
+        id: str,
         title: str,
-        description: str, 
-        author: str, 
-        created_timestamp: float = time.time(), 
-        label_magnitudes: dict = None
-        ) -> None:
+        description: str,
+        author: str,
+        created_timestamp: float = time.time(),
+        label_magnitudes: dict = None,
+    ) -> None:
 
         self.id = id
         self.title = title
@@ -25,23 +26,23 @@ class Chamber:
         self.created_timestamp = created_timestamp
         self.label_magnitudes = label_magnitudes
         return
-    
+
     def get_id(self) -> str:
         return self.id
-    
+
     def get_json_body(self) -> dict:
         body = {
             "created_timestamp": self.created_timestamp,
             "description": self.description,
             "title": self.title,
-            "author": self.author
+            "author": self.author,
         }
 
         if self.label_magnitudes is not None:
             body["label_magnitudes"] = self.label_magnitudes
-        
+
         return body
-    
+
     def __str__(self):
         return f"Chamber(id='{self.id}', title='{self.title}', description='{self.description}', \
             author='{self.author}', created_timestamp='{self.created_timestamp}', \
